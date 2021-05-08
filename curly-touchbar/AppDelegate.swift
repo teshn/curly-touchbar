22598bdc0ecc0d1ac6ca37541e46daedd224d3d8
teshn <83846663+teshn@users.noreply.github.com>
2021-05-09 00:44:03 +0200

    Initial Commit

---
+++ curly-touchbar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  curly-touchbar
+//
+//  Created by Teshn on 09.05.21.
+//  Copyright © 2021 Teshn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ curly-touchbar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  curly-touchbar
+//
+//  Created by Teshn on 09.05.21.
+//  Copyright © 2021 Teshn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
